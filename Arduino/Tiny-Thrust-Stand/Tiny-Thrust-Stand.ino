8e0a3218d29752a9be8d53ce890589c060b78333
Alexey Stankevich <alexey.stn@gmail.com>
2019-05-12 09:37:23 +0300

    Move main cycle to loop()

--- Arduino/Tiny-Thrust-Stand/Tiny-Thrust-Stand.ino
+++ Arduino/Tiny-Thrust-Stand/Tiny-Thrust-Stand.ino
@@ -4,6 +4,7 @@
 #define CURRENT_PIN A0
 #define VOLTAGE_PIN A1
 #define PWM_PIN 10
+#define LED_PIN 13
 #define DT_PIN  3
 #define SCK_PIN 4
 
@@ -37,8 +38,11 @@ void setup()
   pinMode(SCK_PIN, OUTPUT);
   TCCR1B &= 0xF8; // 3.9kHz PWM
   TCCR1B |= 0x02;
-
   Serial.begin(115200);
+}
+
+void loop() 
+{
   uint8_t cmdByte = 0;
   while (cmdByte != 'S')
     if (Serial.available() > 0)
@@ -46,21 +50,19 @@ void setup()
   
   for (uint8_t pwm = 0; pwm < 160; pwm+=10) {
     analogWrite(PWM_PIN, pwm);
+    digitalWrite(LED_PIN, HIGH);
     delay(200);
+    digitalWrite(LED_PIN, LOW);
     hx711GetValue(); // dummy read
     for (uint8_t j = 0; j < 8; j++) {
       uint16_t voltage = analogRead(VOLTAGE_PIN);
       uint16_t current = analogRead(CURRENT_PIN);
       uint32_t pull = hx711GetValue()>>4;
-      sprintf(str, "%4d%6d%6d%10d", pwm, voltage, current, pull);
-      Serial.println(str);
+      sprintf(str, "%4d%6d%6d%10d\n", pwm, voltage, current, pull);
+      Serial.print(str);
     }
   }
   
   analogWrite(PWM_PIN, 0);
   Serial.println("Done");
-}
-
-void loop() 
-{
 }
